c455ade33d7b1e58eeec796d6462fd2e6713bf49
Jonathan Tweedy <jetweedy@users.noreply.github.com>
2020-11-16 20:41:24 -0500

    Add files via upload

--- jet-footnotes.php
+++ jet-footnotes.php
@@ -14,27 +14,23 @@ Author URI: https://jonathantweedy.com
 
 add_filter( 'the_title', 'do_shortcode' );
 
-$jet_footnote_count = 0;
-$jet_footnotes = [];
+$GLOBALS['jet_footnote_count'] = 0;
+$GLOBALS['jet_footnotes'] = [];
 
 function jet_footnote( $atts, $templatecontent ){
-    global $jet_footnote_count;
-    global $jet_footnotes;
-    $jet_footnote_count++;
+    $GLOBALS['jet_footnote_count']++;
     $x = new stdClass();
-    $x->num = $jet_footnote_count;
+    $x->num = $GLOBALS['jet_footnote_count'];
     $x->slug = sanitize_title($atts['slug']);
-    $x->text = $templatecontent;
-    $jet_footnotes[] = $x;
-    return "<sup class='jet-footnote'><a class='jet-footnote' href='#jet-footnote-".$x->slug."'>".$jet_footnote_count."</a></sup>";
+    $x->text = html_entity_decode($templatecontent);
+    $GLOBALS['jet_footnotes'][] = $x;
+    return "<sup class='jet-footnote'><a class='jet-footnote' href='#jet-footnote-".$x->slug."'>".$GLOBALS['jet_footnote_count']."</a></sup>";
 }
 add_shortcode( 'jet-footnote', 'jet_footnote' );
 
 function jet_footnotes( $atts, $templatecontent ){
-    global $jet_footnote_count;
-    global $jet_footnotes;
     $html = "<div class='jet-footnotes'>";
-    foreach($jet_footnotes as $jetfn) {
+    foreach($GLOBALS['jet_footnotes'] as $jetfn) {
         $html .= "<a id='jet-footnote-".$jetfn->slug."'></a><p><sup>".$jetfn->num."</sup>" . $jetfn->text . "</p>";
     }
     $html .= "</div>";
